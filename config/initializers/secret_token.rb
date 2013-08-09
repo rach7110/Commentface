a0c9778b258c664581a5e989c1ce60e8e66b73d3
Gilbert <gilbertbgarza@gmail.com>
2013-08-09 02:04:19 -0500

    Generate rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookface::Application.config.secret_token = '05613e2dc86c11aced44927213e955042f1fab8fbd9c9e4896e2a39fdf04dd30904567f5cf4446fdefef323727a4593dfc4cdf32e904d1c73a4482a1570b745f'
